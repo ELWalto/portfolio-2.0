9075853e87d57df84b6ef1d87613baeac922a7c8
Walter Carvalho <walterclean@gmail.com>
2021-11-12 15:37:55 -0300

    Tls False

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.secret_key = 'portfolio2'
 mail_settings = {
     "MAIL_SERVER": 'smtp.gmail.com',
     "MAIL_PORT": 465,
-    "MAIL_USE_TLS": True,
+    "MAIL_USE_TLS": False,
     "MAIL_USE_SSL": True,
     "MAIL_USERNAME": os.getenv("EMAIL"),
     "MAIL_PASSWORD": os.getenv("SENHA")
